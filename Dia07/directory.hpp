2030618d573d00230cc3a596c2369b0726fe7b6c
DisableGraphics <elchifladod@gmail.com>
2022-12-07 20:04:22 +0100

    Añadido el día 7

--- Dia07/directory.hpp
+++ Dia07/directory.hpp
@@ -29,22 +29,51 @@ class Directory
                 children_files[size] = tmp;
             }
         }
+        void add_children_directory(std::string dir)
+        {
+            if(!dir_exists_in_children(dir))
+            {
+                children_directories.push_back(dir);
+            }
+        }
+        std::vector<std::string> get_child_directories()
+        {
+            return children_directories;
+        }
         unsigned long calculate_size()
         {
-            unsigned long size;
+            size = 0;
             for(auto &p : children_files)
             {
                 size += p.first;
             }
-            /*for(auto &d : children_directories)
-            {
-                size += d->calculate_size();
-            }*/
             return size;
+        }
+        void add_extra_size(unsigned long i)
+        {
+            size += i;
+        }
+        unsigned long get_size()
+        {
+            return size;
+        }
+        std::string get_path()
+        {
+            return path;
         }
 
     private:
+        unsigned long size{0};
         std::string path;
         std::map<int, std::string> children_files;
         std::vector<std::string> children_directories;
+        bool dir_exists_in_children(std::string dir)
+        {
+            for(auto &d : children_directories)
+            {
+                if(dir == d)
+                    return true;
+            }
+            return false;
+        }
 };
--- Dia07/main.cpp
+++ Dia07/main.cpp
@@ -2,13 +2,45 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <memory>
+#include <algorithm>
 #include "directory.hpp"
+
+/*
+    Este programa está dedicado
+    a mi profesor de programación,
+    por suspenderme la práctica porque no añadí
+    7000 funciones auxiliares y la hice
+    limpiamente con un sólo bucle.
+
+    Hay que fastidiarse.
+*/
 
 std::string go_up(std::string directory)
 {
-    directory.pop_back();
-    auto last_slash_pos = directory.find_first_of("/");
-    return directory.substr(0, last_slash_pos-1);
+    if(directory == "/")
+    {
+        return "/";
+    }
+    unsigned long slashpos{0};
+    for(auto i{directory.size()-2}; i > 0; i--)
+    {
+        if(directory[i] == '/')
+        {
+            slashpos = i;
+            break;
+        }
+    }
+    std::string ret{""};
+    for(unsigned long i{0}; i <= slashpos; i++)
+    {
+        ret += directory[i];
+    }
+    if(ret == "")
+    {
+        return "/";
+    }
+    return ret;
 }
 
 std::string go_to_dir(std::string current_dir, std::string dir)
@@ -20,12 +52,14 @@ std::string go_to_dir(std::string current_dir, std::string dir)
 
 int main()
 {
-    std::ifstream in("inputt");
+    std::ifstream in("input");
     std::string line;
     std::string current_directory{""};
-    std::map<std::string, Directory> directories; 
+    std::map<std::string, std::unique_ptr<Directory>> directories; 
     bool parsing_dir_contents{false};
-    int dir_size_count{0};
+    unsigned long dir_size_count{0};
+    unsigned long total_size{70000000};
+    int line_n{0};
     while(std::getline(in, line))
     {
         //It is a command
@@ -39,16 +73,84 @@ int main()
                 }
                 else
                 {
-                    
+                    std::stringstream s{line};
+                    std::string tmp;
+                    s >> tmp;
+                    s >> tmp;
+                    s >> tmp;
+                    current_directory = go_to_dir(current_directory, tmp);
+
+                }
+            }
+            if(line.find("ls") != std::string::npos)
+            {
+                directories[current_directory] = std::make_unique<Directory>(*new Directory(current_directory));
+                if(current_directory != "/")
+                {
+                    auto dir_check = current_directory;
+                    if(current_directory != "/")
+                    {
+                        while(directories.find(go_up(dir_check)) != directories.end())
+                        {
+                            directories[go_up(dir_check)]->add_children_directory(current_directory);
+                            if(dir_check != "/")
+                            {
+                                dir_check = go_up(dir_check);
+                            }
+                            else 
+                            {
+                                break;
+                            }
+                        }
+                    }
                 }
             }
         }
         else 
         {
-           
+            
+            directories[current_directory]->add_element(line);
+            
         }
-        std::cout << line << std::endl;
+        line_n++;
     }
+
+    for(auto &p : directories)
+    {
+        for(auto &y : p.second->get_child_directories())
+        {
+            p.second->add_extra_size(directories[y]->calculate_size());
+            
+        }
+        if(p.second->get_size() <= 100000)
+        {
+            dir_size_count += p.second->get_size();
+        }
+    }
+    //Part 1
+    std::cout << dir_size_count << std::endl;
+
+    auto freespace = total_size - directories["/"]->get_size();
+    std::vector<std::pair<unsigned long, std::string>> directories_for_sort{};
+    for(auto &direc : directories)
+    {
+        //directories_for_sort[direc.second->get_size()] = direc.first;
+        directories_for_sort.push_back(std::make_pair(direc.second->get_size(), direc.first));
+    }
+    std::sort(directories_for_sort.begin(), directories_for_sort.end());
+    //std::reverse(directories_for_sort.begin(), directories_for_sort.end());
+
+    unsigned long selected_dir;
+    for(auto & dir : directories_for_sort)
+    {
+        if((freespace + dir.first) >= 30000000)
+        {
+            selected_dir = dir.first;
+            break;
+        }
+    }
+    //Part 2
+    std::cout << selected_dir << std::endl;
     
     in.close();
 }
