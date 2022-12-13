eb5a8cb63553d41c8a992214b0fa6bd935be44e7
DisableGraphics <elchifladod@gmail.com>
2022-12-13 14:12:42 +0100

    Sincronizar lo poco que he hecho del día 13

--- Dia13/main.cpp
+++ Dia13/main.cpp
@@ -1,9 +1,38 @@
 #include <iostream>
 #include <fstream>
+#include "list.hpp"
+
+bool is_in_correct_order(List &l1, List &l2)
+{
+
+}
 
 int main()
 {
     std::ifstream in("input");
     std::string line;
+    std::string pair1;
+    std::string pair2;
+    bool pair1_gen{true};
+    while(std::getline(in, line))
+    {
+        if(line == "")
+        {
+
+        }
+        else 
+        {
+            if(pair1_gen)
+            {
+                pair1 = line;
+                pair1_gen = false;
+            }   
+            else 
+            {
+                pair2 = line;
+                pair1_gen = true;
+            }
+        }
+    }
     in.close();
 }
